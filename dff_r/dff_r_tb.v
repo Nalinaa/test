bbe60d1838b3782ab2a7c527bf9a23670a4c9ae5
Nalinaa <114544530+Nalinaa@users.noreply.github.com>
2023-02-25 14:18:03 +0530

    Create dff_r_tb.v

---
+++ dff_r/dff_r_tb.v
@@ -0,0 +1,27 @@
+module dff_r_tb;
+  reg reset,d,clk;
+  wire q;
+  dff_r dut(reset,d,clk,q);
+  initial
+  begin
+    $dumpfile("r_dff.vcd");
+    $dumpvars(1,dff_r_tb);
+  end
+  always #5 clk=~clk;
+   
+  initial
+    begin
+    clk=0;reset=0;
+  #5reset=1;
+    end
+  
+  initial
+  begin
+    $monitor("Time=%t,d=%b,q=%b",$time,d,q);
+    d=0;
+    #3 d=1;
+    #6 d=0;
+    #9 d=1;
+    #100 $finish;
+  end
+endmodule
